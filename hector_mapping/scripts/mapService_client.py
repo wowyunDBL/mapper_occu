ba08828fbb69676b9d5ea79db2f2173ea3dac056
wowyunDBL <wowyunpg@gmail.com>
2021-11-15 21:01:10 +0800

    1115

--- hector_mapping/scripts/mapService_client.py
+++ hector_mapping/scripts/mapService_client.py
@@ -7,14 +7,17 @@ import rospy
 
 
 def occupancy_map_client():
-    rospy.wait_for_service('reset_map')
+    rospy.wait_for_service('/reset_map')
+    print("here")
     try:
-        mapCallback_handler = rospy.ServiceProxy('reset_map', Trigger)
+        mapCallback_handler = rospy.ServiceProxy('/reset_map', Trigger)
+        print("here")
         resp = mapCallback_handler()
+        print("here")
         return resp.success
 
     except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
+        print("Service call failed: %s \n"%e)
 
 if __name__ == "__main__":
-    occupancy_map_client()
+    print("ok? " + occupancy_map_client())
